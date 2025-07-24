eafdefaeb5c49d9b9090596ba709b20b183352ad
Abdul Hanan <hananarain3199@gmail.com>
2025-07-24 08:26:35 +0500

    propTypes

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,7 +1,5 @@
-import Banner from './media-gallery/t-shirt/image8.jpg';
 import './hero.css'
-
-export default function Hero(){
+export default function Hero({heading,Banner}){
     return(
         <>
             <div className='hero__banner_main'>
@@ -10,7 +8,7 @@ export default function Hero(){
                 </div>
                 <div className='hero__text'>
                     <h2 className='hero__heading'>
-                        Welcome To React Learning Series
+                        {heading}
                     </h2>
                     <div className='hero__content'>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Banner from './hero';
-import Header from './header';
+import Banner from './components/hero';
+import Header from './components/header';
+import Banner1 from './media-gallery/t-shirt/image8.jpg'
+import Banner2 from './media-gallery/t-shirt/image9.jpg'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <App />
-    <Banner />
+    <Banner heading={'Welcome To React Learning Series'} Banner={Banner2} />
+    <Banner heading={'Welcome To React Learning Series props'} Banner={Banner1} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
